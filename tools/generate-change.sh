c960f3363362b6b673f112e8eacc447462fe5174
benhallouk <benhallouk@gmail.com>
2016-07-15 19:23:43 +0100

    docs(changelog): fix the way we grap the version
    Apply the tag -l instead of  describe which gives another result

--- tools/generate-change.sh
+++ tools/generate-change.sh
@@ -4,13 +4,13 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-if grep -q $(git describe | grep 'v' | tail -1) ChangeLog.md; then    
+if grep -q $(git tag -l | grep 'v' | tail -1) ChangeLog.md; then    
   echo "${RED}✗ There is no change log to be generated.${NC}"
   exit 1
 else  
     LOG_HEADER="$(head -n 4 ChangeLog.md)\n"
 
-    LOG_BODY="\n## [$(git describe | grep 'v' | tail -1)] - $(date +%Y-%m-%d) \n"
+    LOG_BODY="\n## [$(git tag -l | grep 'v' | tail -1)] - $(date +%Y-%m-%d) \n"
 
     LOG_BODY="$LOG_BODY\n### New Features"
     LOG_BODY="$LOG_BODY\n$(git log --pretty=format:'- %s' --grep 'feat(' | awk -F ':' '{print $0}' | sort $1)\n"
