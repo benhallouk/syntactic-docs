f80bf04407c63eb5d18f65dcb445f3dfba756f40
benhallouk <benhallouk@gmail.com>
2016-07-25 21:14:53 +0100

    refactor(script): move js to the site.js
    move all scripts to the main site.js for better maintainability of the site

--- src/SyntacticDocs/wwwroot/js/site.js
+++ src/SyntacticDocs/wwwroot/js/site.js
@@ -1,1 +1,19 @@
-// Write your Javascript code.
+hljs.initHighlightingOnLoad();
+$(document).ready(function(){
+    $(".content").scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });            
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('.content').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });            
+    $('#back-to-top').tooltip('show');
+    var simplemde = new SimpleMDE({ element: $("#simple-mde-demo")[0] });
+});
